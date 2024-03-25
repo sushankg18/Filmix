bc2afba007242481696fac0771c3c73db3684839
Sushank Gautam <sushankg4@gmail.com>
2024-03-25 20:50:19 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <Box w={'100%'} h={'fit-content'} p={'1rem 2rem'} bgColor={'whitesmoke'}>
+        <Text>HOME</Text>
+        <Text>BOLLYWOOD</Text>
+        <Text>HOME</Text>
+    </Box>
+  )
+}
+
+export default Navbar
