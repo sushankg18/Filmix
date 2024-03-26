7768802c37fd2c8ea6221b2f390b9008473955a2
Sushank Gautam <sushankg4@gmail.com>
2024-03-26 23:06:50 +0530

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <Box w={'100%'} h={'fit-content'} p={'1rem 2rem'} bgColor={'whitesmoke'}>
+    <Box w={'100%'} display={'flex'} justifyContent={'space-between'} h={'fit-content'} p={'1rem 2rem'} bgColor={'whitesmoke'}>
+      <Box>
+        <Text>
+          Logo
+        </Text>
+      </Box>
+
+      <Box display={'flex'} gap={'2rem'}>
         <Text>HOME</Text>
         <Text>BOLLYWOOD</Text>
         <Text>HOME</Text>
+      </Box>
     </Box>
   )
 }
